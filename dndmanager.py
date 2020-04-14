b091162c4f165c1118382b95996fd935f2b9d234
TheAngryPopo <63618212+TheAngryPopo@users.noreply.github.com>
2020-04-13 20:25:44 -0400

    made grid

--- dndmanager.py
+++ dndmanager.py
@@ -9,8 +9,8 @@ import random
 #initialize pygame  
 pygame.init()
 
-screenWidth = 800
-screenHeight = 600
+screenWidth = 1000
+screenHeight = 1000
 window = pygame.display.set_mode((screenWidth,screenHeight)) 
   
 pygame.display.set_caption("Submarine Dive")
@@ -23,10 +23,34 @@ green = (25,255,25)
 blue = (25,25,255)
 
 mainMenuBack = (0,162,168)
-  
+
 clock = pygame.time.Clock()
 
+font = pygame.font.Font('freesansbold.ttf', 20)
+fontL = pygame.font.Font('freesansbold.ttf', 35)
+fontXL = pygame.font.Font('freesansbold.ttf', 50)
+
+gameOver = False
+gameLoop = False
+mainMenu = True
+
 prevPressed1 = False
+
+class Sprite:
+    def __init__(self,x,y,width,height): 
+        
+        self.x=x
+        self.y=y
+        self.width=width
+        self.height=height
+  
+    def render(self):
+        global prevSubImg
+        global fishSchoolImg
+        
+        pygame.draw.rect(window,black,(self.x,self.y,self.width,self.height))
+
+Sprite1 = Sprite(100,100,100,100)
 
 def hoverDetection(x,y,w,h):
     global prevPressed1
@@ -40,109 +64,44 @@ def hoverDetection(x,y,w,h):
 
     prevPressed1 = pressed1
 
-class Sprite:
-    def __init__(self,x,y,width,height): 
-        
-        self.x=x
-        self.y=y
-        self.width=width
-        self.height=height
-  
-    def render(self,moveX,moveY):
-        global prevSubImg
-        global fishSchoolImg
-        global Sprite1
-        global obstacles
-        for i in obstacles:
-            x1 = self.x
-            y1 = self.y
-            w1 = self.width
-            h1 = self.height
-            if(i.x+i.width>=x1>=i.x and i.y+i.height>=y1>=i.y): #top left of self touches
-                if self.x <= i.x+(100-(abs(moveX)+1)):
-                    self.y = i.y+i.height
-                elif self.y <= i.y+(100-(abs(moveY)+1)):
-                    self.x = i.x+self.width
-            elif(i.x+i.width>=x1+w1>=i.x and i.y+i.height>=y1>=i.y): #top right of self touches
-                if self.x >= i.x-(100-(abs(moveX)+1)-random.randint(1,5)):
-                    self.y = i.y+i.height
-                elif self.y <= i.y+(100-(abs(moveY)+1)):
-                    self.x = i.x-self.width
-            elif(i.x+i.width>=x1>=i.x and i.y+i.height>=y1+h1>=i.y): #bottom left of self touches
-                if self.x <= i.x+(100-(abs(moveX)+1)):
-                    self.y = i.y-self.height
-                elif self.y <= i.y+(100-(abs(moveY)+1)):
-                    self.x = i.x+self.width
-            elif(i.x+i.width>=x1+w1>=i.x and i.y+i.height>=y1+h1>=i.y): #bottom right of self touches
-                if self.x >= i.x-(100-(abs(moveX)+1)-random.randint(1,5)):
-                    self.y = i.y-self.height
-                elif self.y <= i.y+(100-(abs(moveY)+1)):
-                    self.x = i.x-self.width
+def gridLines():
+    for i in range(0,screenWidth,int(round(screenWidth/10,0))):
+        pygame.draw.rect(window,black,(i,0,1,screenHeight))
 
-Sprite1 = Sprite(100,100,100,100)
-Sprite2 = Sprite(500,500,100,100)
-
-obstacles = {Sprite2}
-
-font = pygame.font.Font('freesansbold.ttf', 20)
-fontL = pygame.font.Font('freesansbold.ttf', 35)
-fontXL = pygame.font.Font('freesansbold.ttf', 50)
-
-gameOver = False
-gameLoop = False
-mainMenu = True
+    for i in range(0,screenHeight,int(round(screenHeight/10,0))):
+        pygame.draw.rect(window,black,(0,i,screenWidth,1))
 
 def GameLoop():
+    
     #variable assignment (probably a more efficient way to import all but I couldnt find any)
     global gameLoop
     global gameOver
-    global obsacles
-    
+    gridSnap = True
+    print(round(773,-2))
     start_ticks = pygame.time.get_ticks()
     
     while gameLoop:
+        mouse = pygame.mouse.get_pos()
+        pressed1, pressed2, pressed3 = pygame.mouse.get_pressed()
+        
         for event in pygame.event.get():
             if (event.type==pygame.QUIT): 
                 pygame.quit()
-##            if gameOver == False:
-##                if (event.type==pygame.KEYDOWN): #Movement
-##                    if (event.key==pygame.K_LEFT):
-##                        moveX = -playerSpeed
-##                        left = True
-##                    if (event.key==pygame.K_RIGHT): 
-##                        moveX = playerSpeed
-##                        right = True
-##                    if (event.key==pygame.K_UP): 
-##                        moveY = -playerSpeed
-##                        up = True
-##                    if (event.key==pygame.K_DOWN): 
-##                        moveY = playerSpeed
-##                        down = True
-##            if (event.type==pygame.KEYUP): 
-##                if (event.key==pygame.K_LEFT):
-##                    moveX=0
-##                    left = False
-##                if (event.key==pygame.K_RIGHT): 
-##                    moveX=0
-##                    right = False
-##                if (event.key==pygame.K_UP): 
-##                    moveY=0
-##                    up = False
-##                if (event.key==pygame.K_DOWN): 
-##                    moveY=0
-##                    down = False
-
-
         
-
         window.fill(white)
+        gridLines()
         
-        #collisions = detectCollisions(Sprite1.x,Sprite1.y,Sprite1.width,Sprite1.height)#,Sprite2.x,Sprite2.y,Sprite2.width,Sprite2.height
+        if pressed1 == True and prevPressed1 == False and gridSnap == False:
+            Sprite1.x = mouse[0]-(Sprite1.width/2)
+            Sprite1.y = mouse[1]-(Sprite1.height/2)
+        
+        Sprite1.render()
 
-        Sprite1.render(100,100)
+        prevPressed1 = pressed1
         
+        pygame.display.flip()
         clock.tick(50)
-
+        
     titleScreen()
 
 def titleScreen():
